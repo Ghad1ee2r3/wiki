d90616524aeeb60034afa5cc7f7e817a02e99184
ghadeer <apps02977@gmail.com>
2020-08-30 17:33:44 +0300

    wiki

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Page(models.Model):
+    title =models.CharField(max_length=120)
+    content= models.TextField()
+    last_update =models.DateTimeField(auto_now_add=True)
+
+
+
+    def get_absolute_url(self):
+        return "/pages/detail/%i/" % self.id
+
+    def __str__(self):
+        return self.title
